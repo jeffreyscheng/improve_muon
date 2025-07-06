63b29c7ae82a63738975a6b2ee559382dde32441
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2025-07-05 21:03:34 -0700

    added break for print

--- code/research/minimal_muon_tanh.py
+++ code/research/minimal_muon_tanh.py
@@ -61,6 +61,7 @@ def zeropower_via_tanh_square(G, alpha=128.0):
     orth_res = orth_residual(tanh_alpha_G)
     skew_res = skew_residual(G, tanh_alpha_G)
 
+    torch._dynamo.graph_break()
     print0(f"tanh_res {tanh_res} orth_res {orth_res} skew_res {skew_res}")
 
     return (tanh_alpha_G / tanh_alpha).to(torch.bfloat16)
